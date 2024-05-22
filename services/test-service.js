1a6ad4ae5ace1046a8aa98e6447e0e73c3cf54b3
dp3.qwe <dp3.qwe@gmail.com>
2024-05-22 04:42:39 +0000

    setting up project and testing api data

--- services/test-service.js
+++ services/test-service.js
@@ -1,11 +1,11 @@
 const utils = require('../utils/test');
 
-
-
 exports.testService = async () => {
-  const svcData = await utils.retriveTest();
-
-  return svcData;
-
-
-}
+  try {
+    const svcData = await utils.retriveTest();
+    return svcData;
+  } catch (error) {
+    console.error('Error in testService:', error);
+    throw error;
+  }
+};
