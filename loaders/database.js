71d56b0874339af03bcc2d4f0ffa2f10c8ab5b27
dretp <dp3.qwe@gmail.com>
2024-06-05 23:36:41 -0400

    postgres connection
    postgres connection

---
+++ loaders/database.js
@@ -0,0 +1,26 @@
+//const { Pool } =  require('postgres-pool');
+const { Pool ,Client } = require('pg');
+
+
+const client = new Client({
+    host: 'safely-novel-humpback.a1.pgedge.io',
+    database: 'codegreen',
+    user: 'admin',
+    password: '5RDNt70eg1o6WLHw523c75Lo',
+    port: 5432,
+
+});
+
+async function connection(){
+
+    const client = new Client();
+    await client.connect();
+    const res = await client.query('SELECT node_name FROM spock.node');
+    const node_names = res.rows.map((row) => row.node_name);
+    await client.end();
+    console.log(node_names);
+}
+
+connection()
+
+module.exports = client;
